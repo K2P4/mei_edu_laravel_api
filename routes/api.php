56b7ccdbd4f0e1376a13689e36a5cb0bf9373e74
Phyo <you@example.com>
2025-08-06 20:04:40 +0630

    final deploy

--- routes/api.php
+++ routes/api.php
@@ -10,9 +10,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 
 Route::prefix('auth')->group(function () {
